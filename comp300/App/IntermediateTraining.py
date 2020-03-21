405c5136044c666b476c84c953ff3316230e576a
patrick-home <patrick.quinn-2@student.manchester.ac.uk>
2020-03-21 16:18:08 +0000

    Removed GUI scripts not needed

--- comp300/App/IntermediateTraining.py
+++
@@ -1,9 +0,0 @@
-import tkinter
-from comp300.App.TrainPolicyApp import ActiveTrainPolicy
-import pickle
-
-if __name__ == '__main__':
-    root = tkinter.Tk()
-    config = pickle.load(open('/home/patrick/models/fullGuiTest/intermediatePolicy.config', "rb"))
-    gui = ActiveTrainPolicy(root, "something-something", 0, "", "", "", config=config)
-    root.mainloop()
